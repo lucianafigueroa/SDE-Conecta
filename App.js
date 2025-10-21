3b3306b45f659558d8e3713bbb428e8846acd755
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2025-10-21 19:03:30 -0300

    Chore: Actualización para hacer funcional el LogIn y SignIn con Google.
    - Borrado el .env para no propagar secretos.
    - Actualizado el .gitignore para proteger los secretos.
    - Actualizadas las funciones dentro de login y register para que funcione con Google.

--- App.js
+++ App.js
@@ -1,39 +1,58 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Bienvenida from './src/screens/Bienvenida.js';
-import Bienvenida02 from './src/screens/Bienvenida02.js';
-import OnboardingScreen  from './src/screens/OnboardingScreen.js';
-import Bienvenida05 from './src/screens/Bienvenida05.js';
-import Login from './src/screens/Login.js';
-import Registro from './src/screens/Registro.js';
-import Seleccion from './src/screens/Seleccion.js';
-import Registrarse1 from './src/screens/Registrarse1.js';
-import VerificarNumero from './src/screens/VerificarNumero.js';
-import VerificarCodigo from './src/screens/VerificarCodigo.js';
-import InicioCliente from './src/screens/InicioCliente.js';
-import MenuUsuario from './src/screens/MenuUsuario.js';
-import Prestadores from './src/screens/Prestadores.js';
-import Calificaciones from './src/screens/Calificaciones.js';
-import MenuProfesional from './src/screens/MenuProfesional.js';
-import NotificacionesProfesional from './src/screens/NotificacionesProfesional.js';
-import Categorias from './src/screens/Categorias.js';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Bienvenida from "./src/screens/Bienvenida.js";
+import Bienvenida02 from "./src/screens/Bienvenida02.js";
+import OnboardingScreen from "./src/screens/OnboardingScreen.js";
+import Bienvenida05 from "./src/screens/Bienvenida05.js";
+import Login from "./src/screens/Login.js";
+import Registro from "./src/screens/Registro.js";
+import Seleccion from "./src/screens/Seleccion.js";
+import Registrarse1 from "./src/screens/Registrarse1.js";
+import VerificarNumero from "./src/screens/VerificarNumero.js";
+import VerificarCodigo from "./src/screens/VerificarCodigo.js";
+import InicioCliente from "./src/screens/InicioCliente.js";
+import MenuUsuario from "./src/screens/MenuUsuario.js";
+import Prestadores from "./src/screens/Prestadores.js";
+import Calificaciones from "./src/screens/Calificaciones.js";
+import MenuProfesional from "./src/screens/MenuProfesional.js";
+import NotificacionesProfesional from "./src/screens/NotificacionesProfesional.js";
+import Categorias from "./src/screens/Categorias.js";
+
+import * as WebBrowser from "expo-web-browser";
 
 const Stack = createNativeStackNavigator();
 
-// -----------------------------------------------------------------------------
-// LOGICA DE FLUJO PRINCIPAL
-// SIMULACIÓN: En una app real, esta variable se obtendría de AsyncStorage.
-// Cambia a 'false' para probar el flujo de usuario recurrente.
-// -----------------------------------------------------------------------------
-//const IS_FIRST_LAUNCH = true; // true -> Inicia en Bienvenida02 | false -> Inicia en Bienvenida
-//<Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={INITIAL_ROUTE}>
-//        {/* Flujo de Primer Ingreso: 02 -> 03 -> 04 -> 05 -> Bienvenida */}
-//       <Stack.Screen name="Bienvenida02" component={Bienvenida02} />
-//        <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
-//        <Stack.Screen name="Bienvenida05" component={Bienvenida05} />
+WebBrowser.maybeCompleteAuthSession();
 
-//        {/* Flujo Principal/Recurrente: Bienvenida -> Login -> Registro */}
-//const INITIAL_ROUTE = IS_FIRST_LAUNCH ? 'Bienvenida02' : 'Bienvenida';
+const [request, response, promptAsync] = Google.useAuthRequest({
+  webClientId: EXPO_PUBLIC_WEB_CLIENT_ID,
+  androidClientId: EXPO_PUBLIC_ANDROID_CLIENT_ID,
+});
+
+const handleGoogleRegister = async (idToken) => {
+  try {
+    const credential = GoogleAuthProvider.credential(idToken);
+    await signInWithCredential(auth, credential);
+    console.log("Registro con Google exitoso.");
+    navigation.navigate("Seleccion");
+  } catch (error) {
+    console.error("Error al autenticar con Google:", error.message);
+    Alert.alert("Error", "Error al conectar con Google. Inténtalo de nuevo.");
+  }
+};
+
+const handleGoogleLogin = async (idToken) => {
+  try {
+    const credential = GoogleAuthProvider.credential(idToken);
+    await signInWithCredential(auth, credential);
+
+    console.log("Login con Google exitoso.");
+    navigation.navigate("InicioCliente");
+  } catch (error) {
+    console.error("Error al autenticar con Google:", error.message);
+    alert("Error al conectar con Google. Inténtalo de nuevo.");
+  }
+};
 
 export default function App() {
   return (
@@ -56,7 +75,10 @@ export default function App() {
         <Stack.Screen name="Prestadores" component={Prestadores} />
         <Stack.Screen name="Calificaciones" component={Calificaciones} />
         <Stack.Screen name="MenuProfesional" component={MenuProfesional} />
-        <Stack.Screen name="NotificacionesProfesional" component={NotificacionesProfesional} />
+        <Stack.Screen
+          name="NotificacionesProfesional"
+          component={NotificacionesProfesional}
+        />
         <Stack.Screen name="Categorias" component={Categorias} />
       </Stack.Navigator>
     </NavigationContainer>
